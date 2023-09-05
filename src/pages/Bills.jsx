014d0710b5226d78c5cb55ddb563659306f66963
mica <ma.mica.merino@gmail.com>
2023-09-05 20:56:15 -0300

    Validación de formulario de gastos

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -2,19 +2,61 @@ import { useRef, useState } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const Bills = () => {
-  const [bills, setBills] = useLocalStorage("bills", [])
-  const form = useRef();
-  let currentDate = new Date()
+  const initialValue = {
+    date: "",
+    distributor: "",
+    amount: "",
+    paid: "",
+  };
+  const [form, setForm] = useState(initialValue);
+  const [bills, setBills] = useLocalStorage("bills", []);
+  const [errors, setErrors] = useState({});
+  const formulario = useRef();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const fieldsData = Object.fromEntries(new FormData(e.target));
-    setBills([...bills, fieldsData]);
-    form.current.reset();
+    const err = validateFields(form);
+    if (err === null) {
+      setBills([...bills, form]);
+      formulario.current.reset();
+      setForm(initialValue);
+      setErrors({})
+    } else {
+      setErrors(err);
+    }
   };
 
-  const handleClick = () => {
-    form.current.reset();
+  const handleClickCancel = () => {
+    formulario.current.reset();
+    setErrors({})
+  };
+
+  const validateFields = (value) => {
+    let errors = {};
+    let isError = false;
+
+    if (value.date.length <= 0) {
+      errors.date = "Debe completar el campo";
+      isError = true;
+    }
+    if (value.distributor.length <= 0) {
+      errors.distributor = "Debe completar el campo";
+      isError = true;
+    }
+    if (value.amount.length <= 0) {
+      errors.amount = "Debe completar el campo";
+      isError = true;
+    }
+    if (value.paid.length <= 0) {
+      errors.paid = "Debe completar el campo";
+      isError = true;
+    }
+    return isError ? errors : null;
   };
 
   return (
@@ -32,12 +74,12 @@ const Bills = () => {
               </tr>
             </thead>
             <tbody>
-              {bills?.map((e) => (
-                <tr key={e.proveedor}>
-                  <td>{e.fecha}</td>
-                  <td>{e.proveedor}</td>
-                  <td>$ {e.monto}</td>
-                  <td>{e.pago}</td>
+              {bills?.map((e, i) => (
+                <tr key={i}>
+                  <td>{e.date}</td>
+                  <td>{e.distributor}</td>
+                  <td>$ {e.amount}</td>
+                  <td>{e.paid}</td>
                 </tr>
               ))}
             </tbody>
@@ -48,30 +90,57 @@ const Bills = () => {
       </section>
       <section className="form-section">
         <h2 className="header-form">Nuevo gasto</h2>
-        <form ref={form} onSubmit={handleSubmit} className="form-container">
+        <form
+          ref={formulario}
+          onSubmit={handleSubmit}
+          className="form-container"
+        >
           <div className="form-content">
-            <label htmlFor="fecha">Fecha</label>
-            <input id="fecha" name="fecha" type="date" autoComplete="on" />
-          </div>
-          <div className="form-content">
-            <label htmlFor="proveedor">Proveedor</label>
+            <label htmlFor="date">Fecha</label>
             <input
-              id="proveedor"
-              name="proveedor"
+              onChange={handleChange}
+              id="date"
+              name="date"
+              type="date"
+              autoComplete="on"
+            />
+          </div>
+          {errors.date && <p>{errors.date}</p>}
+          <div className="form-content">
+            <label htmlFor="distributor">Proveedor</label>
+            <input
+              onChange={handleChange}
+              id="distributor"
+              name="distributor"
               type="text"
               autoComplete="off"
             />
           </div>
+          {errors.distributor && <p>{errors.distributor}</p>}
           <div className="form-content">
-            <label htmlFor="monto">Monto</label>
-            <input id="monto" name="monto" type="number" autoComplete="off" />
+            <label htmlFor="amount">Monto</label>
+            <input
+              onChange={handleChange}
+              id="amount"
+              name="amount"
+              type="number"
+              autoComplete="off"
+            />
           </div>
+          {errors.amount && <p>{errors.amount}</p>}
           <div className="form-content">
-            <label htmlFor="pago">Medio de pago</label>
-            <input id="pago" name="pago" type="text" autoComplete="off" />
+            <label htmlFor="paid">Medio de pago</label>
+            <input
+              onChange={handleChange}
+              id="paid"
+              name="paid"
+              type="text"
+              autoComplete="off"
+            />
           </div>
+          {errors.paid && <p>{errors.paid}</p>}
           <div className="buttons-container">
-            <button type="button" onClick={handleClick}>
+            <button type="button" onClick={handleClickCancel}>
               Cancelar
             </button>
             <button type="submit" className="btn-add">
